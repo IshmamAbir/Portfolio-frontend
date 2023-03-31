cfc96e3e288b049e154c03f110a374f38fd616cb
イシュマム | Ishmam <ishmam130@gmail.com>
2023-03-31 13:06:06 +0900

    refactor: component separation

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,294 @@
+<template>
+  <!-- start Header area -->
+  <div class="d-none d-xl-block header-style-2">
+    <header
+      class="rn-header-area d-flex align-items-start flex-column left-header-style"
+    >
+      <div class="logo-area">
+        <a href="#home">
+          <img src="@/assets/images/ishmam.jpeg" alt="personal-logo" />
+        </a>
+      </div>
+      <nav id="sideNavs" class="mainmenu-nav navbar-example2">
+        <ul class="primary-menu nav nav-pills">
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two active" href="#home">
+              <!-- <vue-feather type="home" /> -->
+              <i data-feather="home"></i>
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#features">
+              <!-- <vue-feather type="briefcase" /> -->
+              <i data-feather="briefcase"></i>
+              Features
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#portfolio">
+              <!-- <vue-feather type="layers" /> -->
+              <i data-feather="layers"></i>
+              Portfolio
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#resume">
+              <!-- <vue-feather type="users" /> -->
+              <i data-feather="users"></i>
+              Resume
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#clients">
+              <!-- <vue-feather type="user" /> -->
+              <i data-feather="user"></i>
+              Clients
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#pricing">
+              <!-- <vue-feather type="shopping-cart" /> -->
+              <i data-feather="shopping-cart"></i>
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#blog">
+              <!-- <vue-feather type="image" /> -->
+              <i data-feather="image"></i>
+              blog
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#contacts">
+              <!-- <vue-feather type="message-circle" /> -->
+              <i data-feather="message-circle"></i>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="footer">
+        <div class="social-share-style-1">
+          <span class="title">find with me</span>
+          <ul class="social-share d-flex liststyle">
+            <li class="facebook">
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-facebook"
+                >
+                  <path
+                    d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
+                  ></path></svg
+              ></a>
+            </li>
+            <li class="instagram">
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-instagram"
+                >
+                  <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                  <path
+                    d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"
+                  ></path>
+                  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg
+              ></a>
+            </li>
+            <li class="linkedin">
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-linkedin"
+                >
+                  <path
+                    d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+                  ></path>
+                  <rect x="2" y="9" width="4" height="12"></rect>
+                  <circle cx="4" cy="4" r="2"></circle></svg
+              ></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  </div>
+  <!-- start Header area end -->
+
+  <!-- Header Mobile Bar  -->
+  <div class="header-style-2 d-block d-xl-none">
+    <div class="row align-items-center">
+      <div class="col-6">
+        <div class="logo">
+          <a href="#home">
+            <img src="@/assets/images/logo/logo-dark.png" alt="Logo" />
+          </a>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="header-right text-right">
+          <div class="hamberger-menu">
+            <i id="menuBtn" class="feather-menu humberger-menu"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Header Mobile Bar  -->
+
+  <!-- Start Popup Mobile Menu  -->
+  <div class="popup-mobile-menu">
+    <div class="inner">
+      <div class="menu-top">
+        <div class="menu-header">
+          <a class="logo" href="#home">
+            <img
+              src="@/assets/images/logo/logos-circle.png"
+              alt="Personal Portfolio"
+            />
+          </a>
+          <div class="close-button">
+            <button class="close-menu-activation close">
+              <i data-feather="x"></i>
+            </button>
+          </div>
+        </div>
+        <p class="discription">
+          Personal Portfolio Website of Ishmam Abir Chowdhury.
+        </p>
+      </div>
+      <div class="content">
+        <ul class="primary-menu nav nav-pills">
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two active" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#features"
+              >Features</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#portfolio"
+              >Portfolio</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#resume">Resume</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#clients">Clients</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#pricing">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#blog">blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link smoth-animation-two" href="#contacts">Contact</a>
+          </li>
+        </ul>
+        <!-- social sharea area -->
+        <div class="social-share-style-1 mt--40">
+          <span class="title">find with me</span>
+          <ul class="social-share d-flex liststyle">
+            <li class="facebook">
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-facebook"
+                >
+                  <path
+                    d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
+                  ></path></svg
+              ></a>
+            </li>
+            <li class="instagram">
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-instagram"
+                >
+                  <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                  <path
+                    d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"
+                  ></path>
+                  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg
+              ></a>
+            </li>
+            <li class="linkedin">
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-linkedin"
+                >
+                  <path
+                    d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+                  ></path>
+                  <rect x="2" y="9" width="4" height="12"></rect>
+                  <circle cx="4" cy="4" r="2"></circle></svg
+              ></a>
+            </li>
+          </ul>
+        </div>
+        <!-- end -->
+      </div>
+    </div>
+  </div>
+  <!-- End Popup Mobile Menu  -->
+</template>
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
---
+++ src/components/AppSlider.vue
@@ -0,0 +1,66 @@
+<template>
+  <section
+    id="home"
+    class="slider-style-5 rn-section-gap pb--110 align-items-center with-particles bg_image"
+    style="background-image: url('src/assets/images/bg-cover.jpg')"
+    data-black-overlay="7"
+  >
+    <div id="particles-js"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="banner-inner">
+            <div class="thumbnail gradient-border gradient-animation">
+              <img
+                id="border"
+                class="gradient-border"
+                src="@/assets/images/ishmam.jpeg"
+                alt=""
+              />
+            </div>
+            <h1 class="text-light">ISHMAM ABIR CHOWDHURY</h1>
+            <!-- type headline start-->
+            <span class="cd-headline clip is-full-width">
+              <span class="text-light">I am a &ensp;</span>
+              <!-- ROTATING TEXT -->
+              <span class="cd-words-wrapper">
+                <b class="is-visible"> Software Engineer.</b>
+                <b class="is-hidden"> Web Developer.</b>
+                <b class="is-hidden"> Photographer & Videographer.</b>
+              </span>
+            </span>
+            <!-- type headline end -->
+            <div class="col-lg-10 offset-lg-1 mt--60">
+              <p class="has-drop-cap" style="text-align: justify">
+                Basically I am A Bangladeshi Origin Full-time Web Developer and
+                A Software Engineer By Profession Who Is Living In Japan and
+                Also A Photographer & Video Editor By Choice. I am At A Time A
+                Hard Working And Super Lazy. I Can Sleep For 17 Hours Straight &
+                Also Can Code From 10:00 PM to 9:00 AM Continuously. My
+                Potential For You Depends On How You Want To Get It.
+              </p>
+            </div>
+            <div class="button-area">
+              <a class="rn-btn cv-download" href="#contacts"
+                ><span>Download CV</span></a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "AppSlider",
+};
+</script>
+
+<style scoped>
+.cv-download {
+  box-shadow: 0px 0px 19px 20px rgba(33, 21, 21, 0.61) inset;
+  -webkit-box-shadow: 0px 0px 19px 20px rgba(33, 21, 21, 0.61) inset;
+  -moz-box-shadow: 0px 0px 19px 20px rgba(33, 21, 21, 0.61) inset;
+}
+</style>
